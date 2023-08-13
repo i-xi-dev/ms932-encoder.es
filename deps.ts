a60862ab3bd15b26001e3a68cdfc91bc7dd38a1d
i-xi-dev <naoyuki.isono@gmail.com>
2023-08-14 06:30:03 +0900

    2.0.14

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { type uint8 } from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.3.2/mod.ts";
-export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.0/mod.ts";
+export { type uint8 } from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.4.1/mod.ts";
+export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.1/mod.ts";
