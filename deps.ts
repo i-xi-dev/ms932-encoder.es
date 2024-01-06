56497489303d004980a632b4193c700895e1fc9a
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-07 01:39:27 +0900

    2.0.24

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { CodePoint } from "https://raw.githubusercontent.com/i-xi-dev/string.es/3.6.0/mod.ts";
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.3.0/mod.ts";
-export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.10/mod.ts";
+export { CodePoint } from "https://raw.githubusercontent.com/i-xi-dev/string.es/4.0.2/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.2/mod.ts";
+export { _TransformStream } from "https://raw.githubusercontent.com/i-xi-dev/compat.es/1.2.13/mod.ts";
