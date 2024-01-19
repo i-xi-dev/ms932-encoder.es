514ddd4106ab223223078c547dc23cb0cd25161f
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-20 01:13:40 +0900

    2.1.3

--- tests/encoder_stream.test.ts
+++ tests/encoder_stream.test.ts
@@ -24,7 +24,7 @@ Deno.test("Ms932.EncoderStream.prototype.fatal", () => {
   assertStrictEquals(ms932Encoder3.fatal, false);
 });
 
-Deno.test("Ms932.EncoderStream.prototype.writable", async () => {
+Deno.test("Ms932.EncoderStream.prototype.readable,writable - fatal:false", async () => {
   const td = [
     "ABC",
     "あ",
@@ -95,7 +95,7 @@ Deno.test("Ms932.EncoderStream.prototype.writable", async () => {
   );
 });
 
-Deno.test("Ms932.EncoderStream.prototype.writable - 2", async () => {
+Deno.test("Ms932.EncoderStream.prototype.readable,writable - fatal:false(末尾が孤立サロゲート)", async () => {
   const td = [
     "ABC",
     "あ",
@@ -158,6 +158,155 @@ Deno.test("Ms932.EncoderStream.prototype.writable - 2", async () => {
     "0x3F,0x41,0x3F,0x41,0x3F," +
     "0x41,0x41,0x41,0x3F,0x41," +
     "0x00,0x3F,0x00,0x00,0x00";
+
+  assertStrictEquals(
+    [...result].map((e) => "0x" + e.toString(16).toUpperCase().padStart(2, "0"))
+      .join(","),
+    expected,
+  );
+});
+
+Deno.test("Ms932.EncoderStream.prototype.readable,writable - fatal:false, replacementChar:string", async () => {
+  const td = [
+    "ABC",
+    "あ",
+    "\uD867",
+    "",
+    "A",
+
+    "\uD867\uDE3E",
+    "A",
+    "\uDE3E",
+    "A",
+    "AA",
+
+    "\uD867",
+    "\uDE3E",
+    "A",
+    "\u0000",
+    "A",
+  ];
+
+  // deno-lint-ignore no-explicit-any
+  let ti: any;
+  const s = new ReadableStream({
+    start(controller) {
+      let c = 0;
+      ti = setInterval(() => {
+        if (c >= 15) {
+          clearInterval(ti);
+          controller.close();
+          return;
+        }
+        controller.enqueue(td[c]);
+        c = c + 1;
+      }, 10);
+    },
+  });
+
+  await (() => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 200);
+    });
+  })();
+
+  const ms932Encoder1 = new Ms932.EncoderStream({
+    fatal: false,
+    replacementChar: "_",
+  });
+
+  const result = new Uint8Array(20);
+  let written = 0;
+  const ws = new WritableStream({
+    write(chunk) {
+      result.set(chunk, written);
+      written = written + chunk.byteLength;
+    },
+  });
+  await s.pipeThrough(ms932Encoder1).pipeTo(ws);
+  await s.pipeTo(ws);
+
+  const expected = "0x41,0x42,0x43,0x82,0xA0," +
+    "0x5F,0x41,0x5F,0x41,0x5F," +
+    "0x41,0x41,0x41,0x5F,0x41," +
+    "0x00,0x41,0x00,0x00,0x00";
+
+  assertStrictEquals(
+    [...result].map((e) => "0x" + e.toString(16).toUpperCase().padStart(2, "0"))
+      .join(","),
+    expected,
+  );
+});
+
+Deno.test("Ms932.EncoderStream.prototype.readable,writable - fatal:false, replacementChar:string(2)", async () => {
+  const td = [
+    "ABC",
+    "あ",
+    "\uD867",
+    "",
+    "A",
+
+    "\uD867\uDE3E",
+    "A",
+    "\uDE3E",
+    "A",
+    "AA",
+
+    "\uD867",
+    "\uDE3E",
+    "A",
+    "\u0000",
+    "A",
+  ];
+
+  // deno-lint-ignore no-explicit-any
+  let ti: any;
+  const s = new ReadableStream({
+    start(controller) {
+      let c = 0;
+      ti = setInterval(() => {
+        if (c >= 15) {
+          clearInterval(ti);
+          controller.close();
+          return;
+        }
+        controller.enqueue(td[c]);
+        c = c + 1;
+      }, 10);
+    },
+  });
+
+  await (() => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 200);
+    });
+  })();
+
+  const ms932Encoder1 = new Ms932.EncoderStream({
+    fatal: false,
+    replacementChar: "あ",
+  });
+
+  const result = new Uint8Array(30);
+  let written = 0;
+  const ws = new WritableStream({
+    write(chunk) {
+      result.set(chunk, written);
+      written = written + chunk.byteLength;
+    },
+  });
+  await s.pipeThrough(ms932Encoder1).pipeTo(ws);
+  await s.pipeTo(ws);
+
+  const expected = "0x41,0x42,0x43,0x82,0xA0," +
+    "0x82,0xA0,0x41,0x82,0xA0,0x41,0x82,0xA0," +
+    "0x41,0x41,0x41,0x82,0xA0,0x41," +
+    "0x00,0x41,0x00,0x00,0x00," +
+    "0x00,0x00,0x00,0x00,0x00,0x00";
 
   assertStrictEquals(
     [...result].map((e) => "0x" + e.toString(16).toUpperCase().padStart(2, "0"))
